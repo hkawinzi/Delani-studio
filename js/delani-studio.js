8f0c84cbfc3f59733adcf9c95761b82afceaa985
happiness <happinesskawinzi@gmail.com>
2019-07-22 12:40:28 +0300

    add toggle effect to what we do

--- js/delani-studio.js
+++ js/delani-studio.js
@@ -1,11 +1,71 @@
-$(document).ready(function(){
-    $(".hidden").click(function(){
-        $(".hidden").toggle();
-        $(".descript-hide p").toggle();
-        $(".caption").css({
-            "front-weight":"bold",
-            "transition":"ls",
-            "cursor":"pointer"
-        })
-    })
-})
+$(document).ready(function() {
+    $(".click0").click(function(){
+       $(".hide1").slideToggle(1000);
+       $("#hidden1").slideToggle(1200);
+       
+        
+     });
+     $(".click1").click(function(){
+       $(".hide2").slideToggle(1000);
+       $("#hidden2").slideToggle(1200);
+     });
+     $(".click2").click(function(){
+       $(".hide3").slideToggle(1000);
+       $("#hidden3").slideToggle(1200);
+     });
+    });
+    
+    
+     $(document).ready(function(){
+       $("#project4").mouseenter(function(){
+         $("#overlay4").show(); }).mouseleave(function(){
+         $("#overlay4").hide();
+      });
+    
+       $("#project3").mouseenter(function(){
+           $("#overlay3").show();
+       }).mouseleave(function(){
+           $("#overlay3").hide();
+       });
+    
+      $("#project2").mouseenter(function(){
+           $("#overlay2").show();
+       }).mouseleave(function(){
+           $("#overlay2").hide();
+       });
+       $("#project1").mouseenter(function(){
+           $("#overlay1").show();
+       }).mouseleave(function(){
+           $("#overlay1").hide();
+       });
+     });
+     $(document).ready(function(){
+       $("#project5").mouseenter(function(){
+           $("#overlay5").show();
+       }).mouseleave(function(){
+           $("#overlay5").hide();
+       });
+       $("#project6").mouseenter(function(){
+           $("#overlay6").show();
+       }).mouseleave(function(){
+           $("#overlay6").hide();
+       });
+       $("#project7").mouseenter(function(){
+           $("#overlay7").show();
+       }).mouseleave(function(){
+           $("#overlay7").hide();
+       });
+       $("#project8").mouseenter(function(){
+           $("#overlay8").show();
+       }).mouseleave(function(){
+           $("#overlay8").hide();
+       });
+     });
+
+     function contact (form) {
+         var name = document.forms["form2"]["name"].value;
+         var email = document.forms["form2"]["email"].value;
+         var message = document.forms["form2"] ["message"].value;
+         alert("Hello! " + name + "," + " We have received your message. Thank you for reaching out to us. ");
+             };
+     
