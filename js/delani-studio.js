e94a53e1cf6708216a9b9eb0e3587b9ca95c8a14
happiness <happinesskawinzi@gmail.com>
2019-07-22 09:37:44 +0300

    addition of jQuery and js

--- js/delani-studio.js
+++ js/delani-studio.js
@@ -1,11 +1,247 @@
-$(document).ready(function(){
-    $(".hidden").click(function(){
-        $(".hidden").toggle();
-        $(".descript-hide p").toggle();
-        $(".caption").css({
-            "front-weight":"bold",
-            "transition":"ls",
-            "cursor":"pointer"
-        })
-    })
-})
+// // $(document).ready(function(){
+// //   $(".bold-center").css("text-align", "center");
+// //   $(".bold-center").css("font-weight", "bold");
+
+// //   $(".hidden").css("text-align", "center");
+// //   $(".hidden").css("margin", "5%");
+$(document).ready(function() {
+$(".click0").click(function(){
+   $(".hide1").slideToggle(1000);
+   $("#hidden1").slideToggle(1200);
+ });
+ $(".click1").click(function(){
+   $(".hide2").slideToggle(1000);
+   $("#hidden2").slideToggle(1200);
+ });
+ $(".click2").click(function(){
+   $(".hide3").slideToggle(1000);
+   $("#hidden3").slideToggle(1200);
+ });
+});
+
+
+//   $(document).ready(function(){
+//   $("#project4").mouseenter(function(){
+//     $("#overlay4").show();
+//   }).mouseleave(function(){
+//     $("#overlay4").hide();
+//   });
+
+//   $("#project3").mouseenter(function(){
+//       $("#overlay3").show();
+//   }).mouseleave(function(){
+//       $("#overlay3").hide();
+//   });
+
+//   $("#project2").mouseenter(function(){
+//       $("#overlay2").show();
+//   }).mouseleave(function(){
+//       $("#overlay2").hide();
+//   });
+//   $("#project1").mouseenter(function(){
+//       $("#overlay1").show();
+//   }).mouseleave(function(){
+//       $("#overlay1").hide();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#project5").mouseenter(function(){
+//       $("#overlay5").show();
+//   }).mouseleave(function(){
+//       $("#overlay5").hide();
+//   });
+//   $("#project6").mouseenter(function(){
+//       $("#overlay6").show();
+//   }).mouseleave(function(){
+//       $("#overlay6").hide();
+//   });
+//   $("#project7").mouseenter(function(){
+//       $("#overlay7").show();
+//   }).mouseleave(function(){
+//       $("#overlay7").hide();
+//   });
+//   $("#project8").mouseenter(function(){
+//       $("#overlay8").show();
+//   }).mouseleave(function(){
+//       $("#overlay8").hide();
+//   });
+// });
+// });
+// $(document).ready(function(){
+//   $("form").submit(function(){
+//       var name = $("input#name").val();
+//       var email = $("input#email").val();
+//       var message = $("textarea#message").val();
+//       if($("input#name").val() && $("input#email").val() && $("textarea#message").val() !== "") {
+//           alert(name + ", we have recieved your message. Thank you for reaching out to us.");
+//       } else {
+//           alert("Please enter correct information on the appropriate fields and try again.");
+//       };
+//   });
+// });
+
+// $(document).ready(function(){
+//     $("#design").click(function(){
+//         $("#hi")$(document).ready(function(){
+            // //   $(".bold-center").css("text-align", "center");
+            // //   $(".bold-center").css("font-weight", "bold");
+            
+            // //   $(".hidden").css("text-align", "center");
+            // //   $(".hidden").css("margin", "5%");
+            
+            //  $(document).ready(function() {
+            //     $("#design").click(function(){
+            //         $("#hidden1").show();
+            //     }); 
+            //  });
+              
+            
+            //   // $(".hide2").click(function(){
+            //   //     $("#hidden2").slideToggle("slow");
+            //   // });
+            
+            //   //   $(".hide3").click(function(){
+            //   //     $("#hidden3").slideToggle("slow");
+            //   // });
+            
+            
+            //   $(document).ready(function(){
+            //   $("#project4").mouseenter(function(){
+            //     $("#overlay4").show();
+            //   }).mouseleave(function(){
+            //     $("#overlay4").hide();
+            //   });
+            
+            //   $("#project3").mouseenter(function(){
+            //       $("#overlay3").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay3").hide();
+            //   });
+            
+            //   $("#project2").mouseenter(function(){
+            //       $("#overlay2").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay2").hide();
+            //   });
+            //   $("#project1").mouseenter(function(){
+            //       $("#overlay1").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay1").hide();
+            //   });
+            // });
+            // $(document).ready(function(){
+            //   $("#project5").mouseenter(function(){
+            //       $("#overlay5").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay5").hide();
+            //   });
+            //   $("#project6").mouseenter(function(){
+            //       $("#overlay6").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay6").hide();
+            //   });
+            //   $("#project7").mouseenter(function(){
+            //       $("#overlay7").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay7").hide();
+            //   });
+            //   $("#project8").mouseenter(function(){
+            //       $("#overlay8").show();
+            //   }).mouseleave(function(){
+            //       $("#overlay8").hide();
+            //   });
+            // });
+            // });
+            // $(document).ready(function(){
+            //   $("form").submit(function(){
+            //       var name = $("input#name").val();
+            //       var email = $("input#email").val();
+            //       var message = $("textarea#message").val();
+            //       if($("input#name").val() && $("input#email").val() && $("textarea#message").val() !== "") {
+            //           alert(name + ", we have recieved your message. Thank you for reaching out to us.");
+            //       } else {
+            //           alert("Please enter correct information on the appropriate fields and try again.");
+            //       };
+            //// $(document).ready(function(){
+// //   $(".bold-center").css("text-align", "center");
+// //   $(".bold-center").css("font-weight", "bold");
+
+// //   $(".hidden").css("text-align", "center");
+// //   $(".hidden").css("margin", "5%");
+
+//  $(document).ready(function() {
+//     $("#design").click(function(){
+//         $("#hidden1").show();
+//     }); 
+//  });
+  
+
+//   // $(".hide2").click(function(){
+//   //     $("#hidden2").slideToggle("slow");
+//   // });
+
+//   //   $(".hide3").click(function(){
+//   //     $("#hidden3").slideToggle("slow");
+//   // });
+
+
+//   $(document).ready(function(){
+//   $("#project4").mouseenter(function(){
+//     $("#overlay4").show();
+//   }).mouseleave(function(){
+//     $("#overlay4").hide();
+//   });
+
+//   $("#project3").mouseenter(function(){
+//       $("#overlay3").show();
+//   }).mouseleave(function(){
+//       $("#overlay3").hide();
+//   });
+
+//   $("#project2").mouseenter(function(){
+//       $("#overlay2").show();
+//   }).mouseleave(function(){
+//       $("#overlay2").hide();
+//   });
+//   $("#project1").mouseenter(function(){
+//       $("#overlay1").show();
+//   }).mouseleave(function(){
+//       $("#overlay1").hide();
+//   });
+// });
+// $(document).ready(function(){
+//   $("#project5").mouseenter(function(){
+//       $("#overlay5").show();
+//   }).mouseleave(function(){
+//       $("#overlay5").hide();
+//   });
+//   $("#project6").mouseenter(function(){
+//       $("#overlay6").show();
+//   }).mouseleave(function(){
+//       $("#overlay6").hide();
+//   });
+//   $("#project7").mouseenter(function(){
+//       $("#overlay7").show();
+//   }).mouseleave(function(){
+//       $("#overlay7").hide();
+//   });
+//   $("#project8").mouseenter(function(){
+//       $("#overlay8").show();
+//   }).mouseleave(function(){
+//       $("#overlay8").hide();
+//   });
+// });
+// });
+// $(document).ready(function(){
+//   $("form").submit(function(){
+//       var name = $("input#name").val();
+//       var email = $("input#email").val();
+//       var message = $("textarea#message").val();
+//       if($("input#name").val() && $("input#email").val() && $("textarea#message").val() !== "") {
+//           alert(name + ", we have recieved your message. Thank you for reaching out to us.");
+//       } else {
+//           alert("Please enter correct information on the appropriate fields and try again.");
+//       };
+//   });
+// });   });
+        
