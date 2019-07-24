86bd67d93744de2ccdcd30bbf180f5f41bac9966
happiness <happinesskawinzi@gmail.com>
2019-07-24 05:02:18 +0300

    js file

--- js/delani-studio.js
+++ js/delani-studio.js
@@ -1,60 +1,81 @@
 $(document).ready(function() {
 $(".click0").click(function(){
-   $("#hidden1").slideToggle(1200);
-   
-    
+   $("#hidden1").slideToggle(4300);
  });
+
+ $(document).ready(function(){
  $(".click1").click(function(){
-   $("#hidden2").slideToggle(1200);
+   $("#hidden2").slideToggle(4300);
  });
+});
+
+$(document).ready(function(){
  $(".click2").click(function(){
-   $("#hidden3").slideToggle(1200);
+   $("#hidden3").slideToggle(4300);
  });
 });
 
 
  $(document).ready(function(){
-   $("#project4").mouseenter(function(){
-     $("#overlay4").show(); }).mouseleave(function(){
-     $("#overlay4").hide();
+   $("#project1").mouseenter(function(){
+     $("#overlay1").show(); 
+    }).mouseleave(function(){
+     $("#overlay1").hide();
   });
+ });
 
-   $("#project3").mouseenter(function(){
-       $("#overlay3").show();
-   }).mouseleave(function(){
-       $("#overlay3").hide();
-   });
-
-  $("#project2").mouseenter(function(){
+  $(document).ready(function(){
+   $("#project2").mouseenter(function(){
        $("#overlay2").show();
    }).mouseleave(function(){
        $("#overlay2").hide();
    });
-   $("#project1").mouseenter(function(){
-       $("#overlay1").show();
+});
+
+   $(document).ready(function(){
+  $("#project3").mouseenter(function(){
+       $("#overlay3").show();
    }).mouseleave(function(){
-       $("#overlay1").hide();
+       $("#overlay3").hide();
+   });
+});
+
+$(document).ready(function(){
+   $("#project4").mouseenter(function(){
+       $("#overlay4").show();
+   }).mouseleave(function(){
+       $("#overlay4").hide();
    });
  });
+
  $(document).ready(function(){
    $("#project5").mouseenter(function(){
        $("#overlay5").show();
    }).mouseleave(function(){
        $("#overlay5").hide();
    });
+});
+
+$(document).ready(function(){
    $("#project6").mouseenter(function(){
        $("#overlay6").show();
    }).mouseleave(function(){
        $("#overlay6").hide();
    });
+});
+
+$(document).ready(function(){
    $("#project7").mouseenter(function(){
        $("#overlay7").show();
    }).mouseleave(function(){
        $("#overlay7").hide();
    });
-   $("#project8").mouseenter(function(){
-       $("#overlay8").show();
-   }).mouseleave(function(){
-       $("#overlay8").hide();
-   });
- });
+});
+
+$(document).ready(function(){
+  $("#project8").mouseenter(function(){
+    $("#overlay8").show();
+  }).mouseleave(function(){
+    $("#overlay8").hide();
+  });  
+});
